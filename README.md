a8f223287df7f92ddad49a7fc2c8d263e0e28480
Oliver Train <olivertrain1996@gmail.com>
2023-06-21 16:50:19 +0100

    Add changes to search criteria and url methods

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Figma Plugin
 
 Notes for steve:
 
-1. Speak to steve regarding logging in and authentication.
-2. Time it takes to load
-3. Results handling - When it searches GOing to need a indicator that it is loading data for the duration, when it gets the results if over how many there are and a escape to load. some form of handling for the user
-4. Look into why the new urlparam syntax is causing a extra = on the end of the query string which i believe to be why we are throwing the issue
+1. Speak to steve regarding logging in and authentication. (seperate account? or user login..... whats preferred?)
+2. Time it takes to load (This is a massive issue most GET requests take 2-4 minutes)
+3. Results handling - When it searches GOing to need a indicator that it is loading data for the duration, when it gets the results if over how many there are and a escape to load. some form of handling for the user what is the max you want to load on plugin window.
+4. Look into why the new urlparam syntax is causing a extra = on the end of the query string which i believe to be why we are not filtering correctly.
+
+
--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 // Connecting to API
-const apiUrl = "https://goodwood.thirdlight.com";
-const authentication = "/rest/v1/auth/loginWithKey";
-const searchPoint = "/rest/v1/search?query="
-const authenticate = apiUrl + authentication;
-const searchEnd = apiUrl + searchPoint;
-let currentSessionId = "";
 const apiKeyToken = "VIdNhL+O7UEa1UKR1mhUhSxG/rtdPH6X"
 const credentials = {
   apiKey: apiKeyToken,
 };
 
+
+// Current API Links
+const apiUrl = "https://goodwood.thirdlight.com";
+const authentication = "/rest/v1/auth/loginWithKey";
+const searchPoint = "/rest/v1/search?query="
+const multiSearch = "/rest/v1/content/multi"
+const authenticate = apiUrl + authentication;
+const searchEnd = apiUrl + searchPoint;
+const multiSearchEnd = apiUrl + multiSearch
+
+// Current Session GLOBAL variables
+let currentSessionId = "";
+let currentSearchIdFields = "";
+let returnedSearchID = [];
 
 fetch(authenticate, {
   method: "POST",
@@ -31,13 +39,18 @@ fetch(authenticate, {
 
 document.getElementById('submit-button').addEventListener("click", searchData);
 
+
+
 function searchData(e) {
   e.preventDefault();
   let query = document.getElementById("query_field").value;
   console.log(query)
-  const searchURL = searchEnd + new URLSearchParams(query)
+  
+  // Encode the URL to % 20 instead of +
+  const encodedQuery = encodeURIComponent(query)
+  const searchURL = searchEnd + encodedQuery;
   console.log(searchURL)
-  fetch(searchEnd + new URLSearchParams(query), {
+  fetch(searchURL, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -46,11 +59,54 @@ function searchData(e) {
   })
     .then(response => response.json())
     .then(data => {
-      console.log(data)
+      // Passing data to response if the length is not greater than 1 it throws a error and a user message
+      if (data.response.length >= 1) {
+        currentSearchIdFields = data.response;
+        iterateFirstTwentyElements();
+      } else {
+        window.alert("I am awfully sorry there is not any results to match try something else!!")
+        throw new Error("That didnt return any results Ooooops!")
+      }
       // Use the session ID for subsequent API requests
   })
   .catch(error => {
       console.error("Error:", error);
   });
 }
+
+
+function iterateFirstTwentyElements() {
+
+  for (var i = 0; i < currentSearchIdFields.length && i < 20; i++) {
+    returnedSearchID.push(currentSearchIdFields[i]);
+  }
+  // assignmentOfId();
+}
+
+
+function assignmentOfId() {
+  let items = ""
+  let searchedId = {
+    itemIds: [
+      items
+    ]
+  }
+  fetch(multiSearchEnd, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Chorus-Session": currentSessionId
+    },
+    body: JSON.stringify(searchedId)
+  })
+    .then(response => response.json())
+    .then(data => {
+
+  })
+  .catch(error => {
+      console.error("Error:", error);
+  });
+
+}
+
 
